56afd0c13227ee5c39f32158c32622bc81627be8
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-02-07 12:51:57 +0900

    wiring #3

--- container/src/main/java/com/poscoict/container/user/User.java
+++ container/src/main/java/com/poscoict/container/user/User.java
@@ -1,8 +1,12 @@
 package com.poscoict.container.user;
 
+import java.util.List;
+
 public class User {
 	private Long no = 0L;
 	private String name = "안대혁";
+	private Friend friend = null;
+	private List<String> friends;
 	
 	public User() {
 	}
@@ -15,7 +19,7 @@ public class User {
 		this.no = no;
 		this.name = name;
 	}
-	
+
 	public void setNo(Long no) {
 		this.no = no;
 	}
@@ -23,9 +27,18 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public void setFriend(Friend friend) {
+		this.friend = friend;
+	}
+
+	
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
 
 	@Override
 	public String toString() {
-		return "User [no=" + no + ", name=" + name + "]";
+		return "User [no=" + no + ", name=" + name + ", friend=" + friend + ", friends=" + friends + "]";
 	}
 }
